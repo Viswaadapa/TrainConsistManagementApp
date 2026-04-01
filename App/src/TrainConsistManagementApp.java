01da17ae8e5078d7c8721b42d86ec3cc549a4897
Viswaadapa <viswaadapa@gmail.com>
2026-04-01 09:17:41 +0530

    UC1: Application initialization

---
+++ App/src/TrainConsistManagementApp.java
@@ -0,0 +1,12 @@
+public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Train Consist Management System ");
+        System.out.println("=================================");
+        System.out.println("Version: 1.0");
+        System.out.println("System Initialized Successfully");
+
+    }
+}
