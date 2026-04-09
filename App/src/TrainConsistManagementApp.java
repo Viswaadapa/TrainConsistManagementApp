bebee0cd81236d7c04dc1f4055efe75541e145ae
Viswaadapa <viswaadapa@gmail.com>
2026-04-09 08:47:31 +0530

    UC19: Binary search for bogie ID using sorted array

--- App/src/TrainConsistManagementApp.java
+++ App/src/TrainConsistManagementApp.java
@@ -3,19 +3,31 @@ public class TrainConsistManagementApp {
     public static void main(String[] args) {
 
         System.out.println("======================================");
-        System.out.println(" UC18 - Linear Search for Bogie ID ");
+        System.out.println(" UC19 - Binary Search for Bogie ID ");
         System.out.println("======================================\n");
 
         String[] bogieIds = {"BG101", "BG102", "BG103", "BG104", "BG105"};
 
-        String searchKey = "BG103";
+        String searchKey = "BG104";
+
+        int low = 0;
+        int high = bogieIds.length - 1;
 
         boolean found = false;
 
-        for (String id : bogieIds) {
-            if (id.equals(searchKey)) {
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            int result = bogieIds[mid].compareTo(searchKey);
+
+            if (result == 0) {
                 found = true;
                 break;
+            } else if (result < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
 
